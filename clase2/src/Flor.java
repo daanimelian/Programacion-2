da881c0b5ea03da8014fb6f082379f7847d9c770
Daniela Melian <meliandaani@gmail.com>
2025-04-01 21:30:29 -0300

    ejercicios clase 2 y clase 4

---
+++ clase2/src/Flor.java
@@ -0,0 +1,52 @@
+/*Implemente una clase Java llamada Flor que tenga tres atributos de tipo String, int
+y float, que respectivamente representen el nombre de la flor, el número de
+pétalos y precio. Se debe incluir un constructor que permita inicializar cada uno
+de los atributos y un constructor que inicialice la flor por defecto con nombre
+Margarita, número de pétalos igual a 10 y precio igual a 34.3. También se deben
+proveer operaciones para modificar los atributos y obtener sus valores en
+forma individual.*/
+
+public class Flor {
+    private String nombre;
+    private int num_petalos;
+    private float precio;
+
+    //Constructores
+
+    public Flor(String nombre, int num_petalos, float precio){
+        nombre=nombre;
+        num_petalos=num_petalos;
+        precio=precio;
+    }
+
+    public Flor(){
+        nombre="Margarita";
+        num_petalos=10;
+        precio=34.4f;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getNum_petalos() {
+        return num_petalos;
+    }
+
+    public void setNum_petalos(int num_petalos) {
+        this.num_petalos = num_petalos;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+}
---
+++ clase4/src/ArrayQueue.java
@@ -0,0 +1,15 @@
+public class ArrayQueue<X> implements Queue<X>{
+    private int f;
+    private int r;
+    private int size;
+    private E[] q;
+
+    public ArrayQueue(int tamanio){
+        f=0;
+        r=0;
+        size=0;
+        q = new E[tamanio];
+
+    }
+
+    }
