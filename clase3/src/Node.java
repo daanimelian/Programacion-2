7c5ee84a45baeb2abe07d6d1d4f25268019f62bf
Daniela Melian <meliandaani@gmail.com>
2025-06-17 16:59:09 -0300

    dfs profe

--- clase3/src/Node.java
+++ clase3/src/Node.java
@@ -5,6 +5,7 @@ public class Node<T> {
     protected T element;
     protected Node<T> next;
     protected Node<T> prev;
+    protected T data;
 
     public Node(T element) {
         this.element = element;
